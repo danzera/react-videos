443cdcf85bb5c6daea3e3cff3e50832c74bde602
Dan Zera <dczera@gmail.com>
2020-05-11 22:08:13 -0700

    add axios and use it to call youtube api on search submit

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,35 @@
+// NODE MODULES
 import React from 'react';
+// CUSTOM MODULES
+import youTube from '../api/youtube';
+// CUSTOM COMPONENTS
 import SearchBar from './search-bar';
+// STYLESHEETS
 import '../scss/app.scss';
 
+// App needs to be a class based component so it can hold the state of our app
 class App extends React.Component {
+	// search function to be passed down to our SearchBar component
+	onSearchSubmit = async searchTerm => {
+		console.log('Search term', searchTerm);
+
+		const response = await youTube.get('/search', {
+			params: {
+				key: process.env.REACT_APP_YOUTUBE_KEY,
+				maxResults: 5,
+				part: 'snippet',
+				q: searchTerm,
+				type: 'video'
+			}
+		});
+
+		console.log('response', response)
+	}
+
 	render() {
 		return (
 			<div className="app ui container">
-				<SearchBar />
+				<SearchBar onSubmit={this.onSearchSubmit} />
 			</div>
 		);
 	}
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
+// SearchBar needs to be a class based component to keep track of the input search term
 class SearchBar extends React.Component {
 	state = {
 		searchTerm: ''
 	}
 
-	onInputChange = (e) => {
+	onInputChange = e => {
 		this.setState({ searchTerm: e.target.value });
 	}
 
-	onFormSubmit = (e) => {
+	onFormSubmit = e => {
 		e.preventDefault();
+		// call the callback from parent component
+		this.props.onSubmit(this.state.searchTerm);
 	}
 
+	// "onSubmit" and "onChange" must be called as such
+	// they are automatically called when form is submitted and the input changes, respectively
 	render() {
 		return (
 			<div className="search-bar ui segment">
