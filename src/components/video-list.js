342fdf88097c0c0a1b445d8f222c094b7d1c21ac
Dan Zera <dczera@gmail.com>
2020-05-11 22:42:31 -0700

    add a video-item component and use it to render the list of videos returned from the youtube api

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoItem from './video-item';
 
-const VideoList = props => {
-	console.log(props);
-
+// destructure props so we can just reference 'videos' directly
+const VideoList = ({ videos }) => {
+	console.log(videos)
+	const videoItemsList = videos.map(video => {
+		return <VideoItem key={video.id.videoId} video={video} />;
+	});
+	
 	return (
-		<div className="video-list">
-			Video List
+		<div className="video-list ui relaxed divided list">
+			{videoItemsList}
 		</div>
 	)
 }
