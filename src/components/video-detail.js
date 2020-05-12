7a3f4558d50855deccd082f9a3888a613fb30858
Dan Zera <dczera@gmail.com>
2020-05-11 23:23:13 -0700

    display video player according to the currently selected video

--- src/components/video-detail.js
+++ src/components/video-detail.js
@@ -4,16 +4,23 @@ const VideoDetail = ({ video }) => {
 	console.log(video)
 	if (!video) {
 		return (
-			<div className="video-detail ui segment">
+			<div>
 				Loading...
 			</div>
 		);
 	}
 
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
 	return (
-		<div className="video-detail ui segment">
-			<h4 className="ui header">{video.snippet.title}</h4>
-			<p>{video.snippet.description}</p>
+		<div className="video-detail">
+			<div className="ui embed">
+				<iframe src={videoSrc} />
+			</div>
+			<div className="ui segment">
+				<h4 className="ui header">{video.snippet.title}</h4>
+				<p>{video.snippet.description}</p>
+			</div>
 		</div>
 	);
 }
