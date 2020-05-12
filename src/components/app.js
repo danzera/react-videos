bb6722334a78db4e355486bcf95d7805b61abdeb
Dan Zera <dczera@gmail.com>
2020-05-11 20:11:48 -0700

    add search-bar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SearchBar from './search-bar';
+import '../scss/app.scss';
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="app">
-				Hey, there.
+			<div className="app ui container">
+				<SearchBar />
 			</div>
 		);
 	}
---
+++ src/components/search-bar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+	state = {
+		searchTerm: ''
+	}
+
+	onInputChange = (e) => {
+		this.setState({ searchTerm: e.target.value });
+	}
+
+	onFormSubmit = (e) => {
+		e.preventDefault();
+	}
+
+	render() {
+		return (
+			<div className="search-bar ui segment">
+				<form className="ui form" onSubmit={this.onFormSubmit}>
+					<div className="field">
+						<label>Search YouTube</label>
+						<input
+							type="text"
+							value={this.state.searchTerm}
+							onChange={this.onInputChange}
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
