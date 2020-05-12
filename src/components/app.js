7a8df1071f26b4d7bcbe88ba28c87f6eca6eb249
Dan Zera <dczera@gmail.com>
2020-05-11 22:18:21 -0700

    add video-list component to be used for displaying list of youtube search results

--- src/components/app.js
+++ src/components/app.js
@@ -4,15 +4,17 @@ import React from 'react';
 import youTube from '../api/youtube';
 // CUSTOM COMPONENTS
 import SearchBar from './search-bar';
+import VideoList from './video-list';
 // STYLESHEETS
 import '../scss/app.scss';
 
 // App needs to be a class based component so it can hold the state of our app
 class App extends React.Component {
+	state = {
+		videos: []
+	}
 	// search function to be passed down to our SearchBar component
 	onSearchSubmit = async searchTerm => {
-		console.log('Search term', searchTerm);
-
 		const response = await youTube.get('/search', {
 			params: {
 				key: process.env.REACT_APP_YOUTUBE_KEY,
@@ -23,13 +25,14 @@ class App extends React.Component {
 			}
 		});
 
-		console.log('response', response)
+		this.setState({ videos: response.data.items });
 	}
 
 	render() {
 		return (
 			<div className="app ui container">
 				<SearchBar onSubmit={this.onSearchSubmit} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
---
+++ src/components/video-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoList = props => {
+	console.log(props);
+
+	return (
+		<div className="video-list">
+			Video List
+		</div>
+	)
+}
+
+export default VideoList;
